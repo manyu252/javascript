3fcee5b9f16d4564fcc184e76cfe1f7742d745f2
manyu252 <42141004+manyu252@users.noreply.github.com>
2018-09-04 00:17:36 +0530

    Add files via upload

--- video.js
+++ video.js
@@ -1,19 +1,33 @@
 (function() {
 	var canvas = document.getElementById('canvas'),
-//	    context = canvas.getContext('2d'),
+	    context = canvas.getContext('2d'),
 	    video = document.getElementById('video'),
 	    vendorURL = window.URL || window.webkitURL;
+	
+	navigator.getMedia = navigator.mediaDevices.getUserMedia || navigator.mozGetUserMedia;
 
-//	navigator.getMedia = navigator.mediaDevices.getUserMedia || navigator.mozGetUserMedia;
-
-	navigator.mediaDevices.getUserMedia({
+	navigator.getUserMedia({
 		video: true,
 		audio: false
 	}, function(stream) {
 		video.src = vendorURL.createObjectURL(stream);
-		video.play();
+		//video.play();
 	}, function(error) {
 		// An error occured
 		// error.code
 	});    
+		
+	video.onplay = function()
+	{
+		canvas.width = video.width;
+		canvas.height = video.height;
+		draw();
+	};
+	
+	function draw()
+	{
+		context.drawImage(video, 150, 200, 100, 100, 0, 0, 100, 100);
+		setTimeout(draw,500);
+	}
+
 })();
